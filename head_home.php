191ae119e50fe11b99ab3910c454072928094778
Malcom MK <malcom.m@live.ca>
2019-09-03 22:09:39 -0400

    changing direction

---
+++ head_home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE>
+<html lang="fr">
+<head>
+
+    <title>Gestionnaire Ticket</title>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href=".\style\style.css">
+    <!-- Google Font   -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+</head>
+<body>
+
+<!--Main Navigation-->
+<div class="bgBuild" id="background-wrapper" data-stellar-background-ratio="0.1" style="background-position: 0% 0%;">
+
+    <div id="navaigation" class="wrapper">
+
+        <div class="header">
+            <div class="header-inner container">
+
+                <div class="row">
+
+                    <div class="col-md-7 ">
+                        <a href="#" class="navbar-brand">
+                            <h1 class="hidden">
+                                <img src="" alt="Solution Afrika logo">
+<!--                            Nom de l'entreprise-->
+                                Solution Afrika
+                            </h1>
+                        </a>
+
+<!--                    Section slogan Header-->
+                        <div class="navbar-slogan" >
+                            Responsive HTML Theme <br>
+                            By Malcom
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-5">
+
+                        <ul class="list-inline user-menu pull-right ">
+                            <li class="user-register  mr-md-3"> <a href="#" class="text-uppercase"> <i class="fa fa-edit mr-1 "></i> Devenir client</a></li>
+                            <li class="user-login  "> <a href="#" class="text-uppercase"><i class="fa fa-user-circle mr-1 "></i> Se connecter</a></li>
+                        </ul>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+</div>
+
+
+<?php
