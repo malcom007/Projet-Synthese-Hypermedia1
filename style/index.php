0f019cb5f4bac1623e4db5581394169ce57cb1e8
Malcom MK <malcom.m@live.ca>
2019-09-11 17:46:09 -0400

    Update index.php
    Testing connexion in DB

--- style/index.php
+++ style/index.php
@@ -1,1 +1,13 @@
 <?php
+require_once ('../classes/Database.php');
+require_once ('../included/config/Config.php');
+
+try
+{
+    $db = Database::getInstance();
+    echo "Cool";
+}
+catch (PDOException $e){
+    echo "erreur de connexion: ";
+}
+
