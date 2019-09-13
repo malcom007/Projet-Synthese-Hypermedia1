4b22083f0329984603e941b3fc7f452f94b710bd
ruskov2050 <onicedimukey@gmail.com>
2019-09-12 22:41:52 -0400

    accueil dashbord

--- Acceuil dashboard.php
+++ Acceuil dashboard.php
@@ -3,51 +3,60 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.ico">
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Light Bootstrap Dashboard - Free Bootstrap 4 Admin Dashboard by Creative Tim</title>
+    <title>swipngo</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-
-
-
-
-
-
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+
     <!-- CSS Files -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
     <link rel="stylesheet" href=".\style\style.accueil.css">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/light-bootstrap-dashboard.css?v=2.0.0 " rel="stylesheet" />
 
 </head>
 <body>
     <div class="wrapper">
         <div class="sidebar" data-color="blue">
+
             <div class="sidebar-wrapper">
                 <div class="logo">
                     <a href="javascript:;" class="simple-text">
                          Logo
                     </a>
                 </div>
+
                 <ul class="nav">
-                    <li class="nav-item active">
+                    <li class="active">
                         <a class="nav-link" href="Acceuil%20dashboard.php">
                            <i class="fa fa-home"></i>
                             <p>Acceuil</p>
+                        </a>
+                    </li>
+                    <li >
+                        <a class="nav-link" href="Acceuil%20dashboard.php">
+                            <i class="fas fa-money-bill-alt"></i>
+                            <p>Acheter Credit</p>
+                        </a>
+                    </li>
+                    <li >
+                        <a class="nav-link" href="Acceuil%20dashboard.php">
+                            <i class="fas fa-exchange-alt"></i>
+                            <p>Transferer Credit</p>
+                        </a>
+                    </li>
+                    <li >
+                        <a class="nav-link" href="Acceuil%20dashboard.php">
+                            <i class="fas fa-history"></i>
+                            <p>Historique</p>
                         </a>
                     </li>
                 </ul>
@@ -70,7 +79,7 @@
 
                             <li class="dropdown nav-item">
                                 <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                                    <i class="nc-icon nc-planet"></i>
+                                    <i class="fas fa-user-circle"></i>
                                     <span class="notification">10</span>
                                     <span class="d-lg-none">Notification</span>
                                 </a>
