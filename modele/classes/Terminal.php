5045766c3729a80584023116e9479adfbef24bd3
Malcom MK <malcom.m@live.ca>
2019-09-21 12:21:00 -0400

    Update Terminal.php

--- modele/classes/Terminal.php
+++ modele/classes/Terminal.php
@@ -3,7 +3,7 @@ require_once '../../modele/IdGenerator.php';
 
 class Terminal
 {
-    private $_IdTerminal,$_Libelle,$_MacAdresse,$_Prix, $_DateAjout;
+    protected $_IdTerminal,$_Libelle,$_MacAdresse,$_Prix, $_DateAjout;
 
 
 
@@ -57,102 +57,6 @@ class Terminal
         //On arrondi a 2chiffre après la virgule
         $this->_Prix=round($tableau['prix'],2);
 
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getIdEntreprise()
-    {
-        return $this->_IdEntreprise;
-    }
-
-    /**
-     * @param mixed $IdEntreprise
-     */
-    public function setIdEntreprise($IdEntreprise)
-    {
-        $this->_IdEntreprise = $IdEntreprise;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getStatut()
-    {
-        return $this->_Statut;
-    }
-
-    /**
-     * @param mixed $Statut
-     */
-    public function setStatut($Statut)
-    {
-        $this->_Statut = $Statut;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLogin()
-    {
-        return $this->_Login;
-    }
-
-    /**
-     * @param mixed $Login
-     */
-    public function setLogin($Login)
-    {
-        $this->_Login = $Login;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getPassword()
-    {
-        return $this->_Password;
-    }
-
-    /**
-     * @param mixed $Password
-     */
-    public function setPassword($Password)
-    {
-        $this->_Password = $Password;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getDateActivation()
-    {
-        return $this->_DateActivation;
-    }
-
-    /**
-     * @param mixed $DateActivation
-     */
-    public function setDateActivation($DateActivation)
-    {
-        $this->_DateActivation = $DateActivation;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getDateModification()
-    {
-        return $this->_DateModification;
-    }
-
-    /**
-     * @param mixed $DateModification
-     */
-    public function setDateModification($DateModification)
-    {
-        $this->_DateModification = $DateModification;
     }
 
 
