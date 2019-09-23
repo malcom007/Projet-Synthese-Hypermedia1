e1b24ec9f5e84ccef91b974346664f15241eec67
Malcom MK <malcom.m@live.ca>
2019-09-23 18:29:42 -0400

    Update TestDaoTransporteur.php

--- vue/transporteur/TestDaoTransporteur.php
+++ vue/transporteur/TestDaoTransporteur.php
@@ -8,18 +8,18 @@ require_once '../../modele/TransporteurDAO.php';
 $test= TransporteurDao::find("");
 var_dump($test);
 
-if (empty($test)){
-    echo "Aucune données";
-}else{
-   foreach ($test as $item){
-       echo $item->getIdtransporteur()."<br>";
-       echo $item->getRaisonSocial()."<br>";
-       echo $item->getTelephone()."<br>";
+//if (empty($test)){
+//    echo "Aucune données";
+//}else{
+//   foreach ($test as $item){
+//       echo $item->getIdtransporteur()."<br>";
+//       echo $item->getRaisonSocial()."<br>";
+//       echo $item->getTelephone()."<br>";
+//
+//    }
+//}
+//var_dump($test);
 
-    }
-}
-var_dump($test);
-/*
 $Trans = new Transporteur();
 
 $Trans->addTransporteur("social","","5142081205");
@@ -27,4 +27,4 @@ var_dump($Trans);
 
 $tran = TransporteurDAO::create($Trans);
 
-var_dump($Trans);*/
+var_dump($Trans);
