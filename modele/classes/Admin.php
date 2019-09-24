684e13e95c54fe39b4205559231b720ac7eaf5f9
Malcom MK <malcom.m@live.ca>
2019-09-24 17:35:51 -0400

    update

---
+++ modele/classes/Admin.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'Personnes.php';
+class Admin extends Personnes {
+
+    public function __construct()
+    {
+    }
+
+    public function createNewAdmin($prenom,$nom,$numCell,$email,$password){
+        parent::createNewPersonWeb($prenom,$nom,$numCell,$email,$password,1);
+    }
+
+
+
+
+
+
+}
